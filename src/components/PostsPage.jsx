d167f37a4d2a11db1fbe447adaac335da92c0c2d
MicheleCasani <casanimichele@gmail.com>
2025-05-19 14:21:38 +0200

    [UPD]  modified PostPage

--- src/components/PostsPage.jsx
+++ src/components/PostsPage.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import PostsList from './PostsList'
 
 const PostsPage = () => {
     return (
-        <div>
-
-        </div>
+        <>
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 justify-content-center text-center my-5">
+                        <h1>Postpage</h1>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-12 justify-content-center text-center">
+                        <PostsList />
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
